3e2dd5296184922ad80bf4eedcdc1d8361c5ea3a
erikwizbo <82509929+erikwizbo@users.noreply.github.com>
2023-11-20 18:28:03 -0500

    Update script.js
    Bubble sort works now

--- script.js
+++ script.js
@@ -14,74 +14,109 @@ function init() {
     showBars(array);
 }
 
-// This is your play function that initiates the sorting animation
 function play() {
-  const swaps = bubbleSort(array); // Get the swaps needed to sort the array
-  animate(swaps, 0); // Start the animation with the first swap
+    const actions = bubbleSort([...array]); // Get the actions needed to sort the array
+    animate(actions, 0); // Start the animation with the first action
 }
 
-
-// Animate the sorting process
-function animate(swaps, index) {
-  if (index < swaps.length) {
-      const [i, j] = swaps[index];
-      showBars(array, [i, j], 'green'); // Highlight bars to be compared
-      
-      setTimeout(() => {
-          // Swap the bars
-          const temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-          
-          showBars(array, [i, j], 'red'); // Highlight bars being swapped
-          
-          setTimeout(() => {
-              showBars(array); // Show bars without highlights
-              animate(swaps, index + 1); // Continue with the next swap
-          }, 300); // Delay for showing the swap
-      }, 300); // Delay for showing the comparison
+function animate(actions, index) {
+    if (index < actions.length) {
+      const action = actions[index];
+  
+      if (action.type === 'compare') {
+        showBars(action.state, action.indices, 'green');
+      } else if (action.type === 'swap') {
+        showBars(action.state, action.indices, 'red');
+      }
+  
+      // Use requestAnimationFrame to call the next step of the animation
+      requestAnimationFrame(() => {
+        setTimeout(() => {
+          animate(actions, index + 1);
+        }, 250);
+      });
+    } else {
+      // Once all swaps and comparisons are done, we start the final pass
+      highlightAllBarsGreen(actions[actions.length - 1].state);
+    }
   }
+  
+
+// Function to sequentially highlight each bar in green and keep it green
+function highlightAllBarsGreen(sortedArr) {
+    let highlightedIndices = [];
+
+    for (let i = 0; i < sortedArr.length; i++) {
+        setTimeout(() => {
+            highlightedIndices.push(i);
+            // Highlight all the bars that have been compared so far
+            showBars(sortedArr, highlightedIndices, 'green');
+        }, i * 200);
+    }
+
+    // After the last comparison, ensure the entire array is highlighted in green
+    setTimeout(() => {
+        showBars(sortedArr, sortedArr.map((_, idx) => idx), 'green');
+    }, (sortedArr.length - 1) * 100);
 }
 
+  
 function bubbleSort(arr) {
-    let swaps = [];
+    let actions = []; // To store actions (comparison and swap) and the state of the array
     let n = arr.length;
     let swapped;
     do {
         swapped = false;
         for (let i = 1; i < n; i++) {
-            // Compare the adjacent positions
+            // Record the comparison
+            actions.push({ type: 'compare', indices: [i - 1, i], state: [...arr] });
+
             if (arr[i - 1] > arr[i]) {
+                // Swap the elements in the array
+                let temp = arr[i - 1];
+                arr[i - 1] = arr[i];
+                arr[i] = temp;
+
                 // Record the swap
-                swaps.push([i - 1, i]);
-                // Swap is needed, set swapped to true
+                actions.push({ type: 'swap', indices: [i - 1, i], state: [...arr] });
                 swapped = true;
             }
         }
-        // After each pass, the largest element has bubbled up to the end,
-        // so the length of the array that needs to be checked decreases by 1
         n--;
-    } while (swapped); // Continue passes until no more swaps are needed
-    return swaps;
+    } while (swapped);
+
+    // Do not forget to add the sorted state as the last action
+    actions.push({ type: 'sorted', state: [...arr] });
+
+    return actions;
 }
 
 
-
-// Modify the showBars function to accept indices and color for highlighting
+// Modify the showBars function to update bars instead of recreating them
 function showBars(arr, highlightIndices = [], highlightColor = "") {
-  container.innerHTML = ""; // Clear the container
-  arr.forEach((value, index) => {
-      const bar = document.createElement("div");
-      bar.style.height = value * 200 + "px"; //scale the bar height
-      bar.classList.add("bar");
+    // If the bars have not been created yet, create them.
+    if (container.children.length === 0) {
+        arr.forEach((value, index) => {
+            const bar = document.createElement("div");
+            bar.style.height = value * 200 + "px";
+            bar.classList.add("bar");
+            container.appendChild(bar);
+        });
+    }
 
-      //if the current bar is one of the bars to be highlighted, change its color
-      if (highlightIndices.includes(index)) {
-          bar.style.backgroundColor = highlightColor;
-      }
+    // Update existing bars
+    arr.forEach((value, index) => {
+        const bar = container.children[index];
+        bar.style.height = value * 200 + "px"; // Update the height of the bar
 
-      container.appendChild(bar);
-  });
+        // Reset the color of all bars
+        bar.style.backgroundColor = '';
+
+        // If the current bar is one of the bars to be highlighted, change its color
+        if (highlightIndices.includes(index)) {
+            bar.style.backgroundColor = highlightColor;
+        }
+    });
 }
 
 
@@ -97,3 +132,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     init(); // Initialize the first set of bars
 });
+
